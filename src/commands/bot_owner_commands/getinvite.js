f0be39d73f0b775343f656cc3e89047cfea87cd9
Matteo <matteo.feyaerts@gmail.com>
2022-06-17 18:24:09 +0200

    some attempt at something...

--- src/commands/bot_owner_commands/getinvite.js
+++ src/commands/bot_owner_commands/getinvite.js
@@ -1,16 +1,33 @@
+
+
 module.exports = {
     name: 'ginvite',
-    description: "pong!",
-    execute(message, args, cmd, client, Discord){
+    description: "get's an invite link to all servers the bot is in",
+    async execute(message, args, cmd, client, Discord){
         if(message.author.id != process.env.OWNER) return message.channel.send(`I don't think this is for you!`)
         
-            client.guilds.cache.forEach(guild => {
-                message.channel.createInvite({ unique: true, temporary: false }).then(invite => {
-                    console.log(invite.code);
-                });
+
+        // let guild = client.guilds.get(args[0]);
+
+        // if (!guild) return message.reply("The bot isn't in the guild with this ID.");
+
+        // let invitechannels = guild.channels.filter(c=> c.permissionsFor(guild.me).has('CREATE_INSTANT_INVITE'))
+        // if(!invitechannels) return message.channel.send('No Channels found with permissions to create Invite in!')
+
+        // invitechannels.random().createInvite()
+        // .then(invite=> message.channel.send('Found Invite:\n' + invite.code))
+        // }
+        // client.guilds.cache.forEach(guild => {
+        //         const ch = guild.channels.cache.filter(x => x.type != "category" && x.permissionsFor(guild.me).has('CREATE_INSTANT_INVITE'))
+        //         ch.random.createInvite()
                 
-            })
+        //         console.log(ch)
+                
+                
+                
+                
+        //     });
         
         
-    }
+}
 }
