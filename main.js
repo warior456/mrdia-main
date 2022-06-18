daaaef9dd5f213d6c8bb8c1515fd7942a6ec1228
Matteo <matteo.feyaerts@gmail.com>
2022-06-18 17:28:11 +0200

    structure changes (using https://github.com/RileCraft/DiscordBot-Template)

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
+// async () => {
+//     const Discord = require("discord.js");
+//     const config = require("./Config");
+//     const path = __dirname;
+//     const client = new Discord.Client({
+//         intents: [
+//             Discord.Intents.FLAGS.GUILDS,
+//             Discord.Intents.FLAGS.GUILD_MESSAGES,
+//             Discord.Intents.FLAGS.GUILD_PRESENCES,
+//             Discord.Intents.FLAGS.DIRECT_MESSAGES,
+//             Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+//             Discord.Intents.FLAGS.GUILD_MEMBERS,
+//             Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+//             Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+//             Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+//             Discord.Intents.FLAGS.GUILD_INVITES,
+//             Discord.Intents.FLAGS.GUILD_BANS
+//         ],
+//         partials: ["CHANNEL"]
+//     });
+//     exports.client = client;
+//     exports.path = path;
+//     exports.config = config;
+//     client.commands = {};
+//     client.events = new Discord.Collection();
+//     client.commands.messageCommands = new Discord.Collection();
+//     client.commands.messageCommands.aliases = new Discord.Collection();
+//     client.commands.contextMenus = new Discord.Collection();
+//     client.commands.slashCommands = new Discord.Collection();
+//     client.commands.buttonCommands = new Discord.Collection();
+//     client.commands.selectMenus = new Discord.Collection();
+        
+//     const Handler = require(`${path}/Src/Structures/Handlers/Handler`);
+//     await Handler.loadMessageCommands(client, path);
+//     await Handler.loadEvents(client);
+//     await client.login(config.token);
+//     await Handler.loadSlashCommands(client, path);
+//     await Handler.loadContextMenus(client, path);
+//     await Handler.loadButtonCommands(client, path);
+//     await Handler.loadSelectMenus(client, path);
+//     }
+
+
+
+
 const Discord = require('discord.js');
-import('node-fetch')    
+import('node-fetch')
 
 const { Client, Intents } = require('discord.js');
 global.client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_INVITES] });
@@ -8,19 +53,19 @@ require('dotenv').config();
 
 require('ffmpeg-static');
 const { Player } = require("discord-music-player");
-require('./src/handlers/loader')
+require('./src/Structures/Handlers/Handler')
 const player = new Player(client, {
     leaveOnEmpty: false,            //optional
 });
 client.player = player;
 
 client.player
-    .on('channelEmpty',  (queue) => console.log(`Everyone left the Voice Channel, queue ended.`))
-    .on('songAdd',  (queue, song) => console.log(`Song ${song} was added to the queue.`))
-    .on('playlistAdd',  (queue, playlist) => console.log(`Playlist ${playlist} with ${playlist.songs.length} was added to the queue.`))
-    .on('queueEnd',  (queue) => console.log(`The queue has ended.`))
+    .on('channelEmpty', (queue) => console.log(`Everyone left the Voice Channel, queue ended.`))
+    .on('songAdd', (queue, song) => console.log(`Song ${song} was added to the queue.`))
+    .on('playlistAdd', (queue, playlist) => console.log(`Playlist ${playlist} with ${playlist.songs.length} was added to the queue.`))
+    .on('queueEnd', (queue) => console.log(`The queue has ended.`))
     .on('songChanged', (queue, newSong, oldSong) => console.log(`${newSong} is now playing.`))
-    .on('songFirst',  (queue, song) => console.log(`Started playing ${song}.`))
+    .on('songFirst', (queue, song) => console.log(`Started playing ${song}.`))
     .on('clientDisconnect', (queue) => console.log(`I was kicked from the Voice Channel, queue ended.`))
     .on('clientUndeafen', (queue) => console.log(`I got undefeanded.`))
     .on('error', (error, queue) => {
--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,18 +1,17 @@
-require('dotenv').config();
-const {guildPrefix} = require('../utilities');
-module.exports = (client, message, Discord) => {
-    const prefix = (process.env.PREFIX);
-    
-    if(!message.content.startsWith(prefix)|| message.author.bot) return;
-    
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const cmd = args.shift().toLowerCase();
-    
-    const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
-
-    try {
-        command.execute(message, args, cmd, client, Discord);
-    } catch (err) {
-        console.log(err);
+module.exports = {
+    name: "messageCreate",
+    run: async(message, client, container) => {
+        const loadCommandOptions = require("../Structures/CommandOptions/loadCommandOptions")
+        container.Config.prefix.forEach(prefix => {
+            if (!message.content.toLowerCase().startsWith(prefix)) return;
+            const cmdName = message.content.toString().toLowerCase().slice(prefix.length).trim().split(" ")[0]
+            const command = client.commands.messageCommands.get(cmdName) ?? client.commands.messageCommands.get(client.commands.messageCommands.aliases.get(cmdName))
+            if (!command) return;
+            if (command.allowBots) loadCommandOptions(client, message, command, false)
+            else if (message.author.bot) return;
+            else if (command.guildOnly == false) loadCommandOptions(client, message, command, false)
+            else if (!message.guild) return;
+            else loadCommandOptions(client, message, command, false)
+        })
     }
 }
