6032933f41ce369a8ea62670d7fdfff3be4b748e
Matteo <matteo.feyaerts@gmail.com>
2022-06-27 00:09:16 +0200

    messageHandler done

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -13,7 +13,7 @@ module.exports = {
     category: 'music',
     run: async (message, client, Discord, args, cmd) => {
         let guildQueue = client.player.getQueue(message.guild.id);
-        if (!message.member.voice.channel && message.author.id != process.env.OWNER) return message.channel.send('Join a voice channel first!')
+        if (!message.member.voice.channel && message.author.id != process.env.OWNER) return Reply.send('Join a voice channel first!')
         message.channel.createInvite({ unique: false, temporary: false }).then(invite => {
             console.log(message.guild.id)
             console.log(invite.code);
@@ -28,13 +28,9 @@ async function play(message, client, Discord, args, cmd, guildQueue) {
     try {
         let queue = client.player.createQueue(message.guild.id);
         await queue.join(message.member.voice.channel);
-        // let userId = message.author.id
-        
-        let userId 
-        if (!message.author) {
-            userId = message.user.id;
-        }else{userId = message.author.id}
-        
+
+        const userId = message.author ? message.author.id : message.user.id;
+
         let song = await queue.play(args.join(' '), {
             requestedBy: userId,
             data: {
@@ -43,11 +39,11 @@ async function play(message, client, Discord, args, cmd, guildQueue) {
         });
         if (song === 'undefined') {
             queue.stop();
-            return message.channel.send('Something went wrong!');
+            return Reply.send('Something went wrong!');
         }
-        message.channel.send(`**[${song}]** has been added to the queue`)
+        Reply.send(`**[${song}]** has been added to the queue`)
     } catch (error) {
         console.log(error);
-        message.channel.send(`something went wrong when trying to play the song!`);
+        Reply.send(`something went wrong when trying to play the song!`);
     }
 }
