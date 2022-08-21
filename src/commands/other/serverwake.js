bace026541797b02924fdcaf6150f94097916dde
matteo2678 <66562258+warior456@users.noreply.github.com>
2022-08-21 20:24:49 +0200

    Update serverwake.js

--- src/commands/other/serverwake.js
+++ src/commands/other/serverwake.js
@@ -7,10 +7,10 @@ module.exports = {
     description: "wakes the Minecraft server",
     category: 'other',
     run: async (message, client, Discord, args, cmd) => {
-        if(message.channel.id != '998168141911822376') return Reply.send(message, 'Wrong channel! (go to: Not a Discord Community But Rather a Discord Server #serverwake)')
+        if(message.channel.id != '998168141911822376') return message.channel.send(message, 'Wrong channel! (go to: Not a Discord Community But Rather a Discord Server #serverwake)')
         wol.wake('E8:40:F2:3D:FB:93', function(err, res){
             console.log(err);
         });
-        await Reply.send(message, "Waking server...")
+        await message.channel.send(message, "Waking server...")
     }
 }
