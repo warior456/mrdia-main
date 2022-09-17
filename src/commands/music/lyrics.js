60b1484ea55fc2c9ffbafa57d542cd5f9f89cb1c
Matteo <matteo.feyaerts@gmail.com>
2022-09-17 22:10:41 +0200

    lyrics done

--- src/commands/music/lyrics.js
+++ src/commands/music/lyrics.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const Reply = require('../../Structures/Handlers/replyHandler')
 const axios = require("axios");
 
@@ -9,22 +10,32 @@ const Genius = require("genius-lyrics");
 module.exports = {
     name: 'lyrics',
     aliases: [],
-    description: 'wip',
+    description: 'Gives the lyrics to a song',
+    options: [{
+        name: "name",
+        type: "STRING",
+        description: "Give the song name",
+        required: true
+    }],
     category: 'music',
     run: async (message, client, Discord, args, cmd) => {
-        return Reply.send('This feature is currently WIP')
+        //return Reply.send('This feature is currently WIP')
         // if (!message.member.voice.channel && message.author.id != process.env.OWNER) return Reply.send(message, 'Join a voice channel first!')
 
         const searches = await LyricsClient.songs.search("sasageyo");
 
         //Pick first one
         const firstSong = searches[0];
-        console.log("About the Song:\n", firstSong, "\n");
-        
-        // Ok lets get the lyrics
+
         const lyrics = await firstSong.lyrics();
-        console.log("Lyrics of the Song:\n", lyrics, "\n");
+        console.log(lyrics)
+        const embed = new MessageEmbed()
+            .setColor('#FFFF00')
+            .setTitle(`**Lyrics of the Song:**`)
+            .setDescription(lyrics)
+
+        Reply.send(message, { embeds: [embed] })
+
+
     }
 }
-
-// 9c7b9116c9msh7ae5f48655b7a57p16be1bjsn21d5d5d4aad2
