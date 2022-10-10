26a3a3db8bef9e044c89cd4e6aa20d789db92bd8
warior456 <matteo.feyaerts@gmail.com>
2022-10-10 13:13:08 +0200

    small fixes and work

--- src/commands/buttons/end.js
+++ src/commands/buttons/end.js
@@ -6,11 +6,11 @@ module.exports = {
     run: async (message, client, container) => {
         message.deferUpdate()
         let row = message.message.components[0]
-        let queEmbed = message.message.embeds[0]
+        let Embed = message.message.embeds[0]
         for (let i = 0; i < row.components.length; i++) {
             row.components[i].setDisabled(true);
         }
-        content = { embeds: [queEmbed], components: [row] }
+        content = { embeds: [Embed], components: [row] }
         Reply.edit(message.message, content);
     }
 }
