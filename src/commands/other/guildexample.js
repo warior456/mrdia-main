a51c738af876dbdf46ada722c747456089e64a47
Matteo <matteo.feyaerts@gmail.com>
2022-11-28 21:52:20 +0100

    database

---
+++ src/commands/other/guildexample.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Guild = require('../../schemas/guild')
+Reply = require('../../Structures/Handlers/replyHandler')
+
+module.exports = {
+    name: 'guilddatabase',
+    aliases: [],
+    description: "guilddatabase",
+    category: 'other',
+    run: async (message, client, Discord, args, cmd) => {
+        let guildProfile = await Guild.findOne({ guildId: message.guild.id })
+        if (!guildProfile) {
+            guildProfile = await new Guild({
+                _id: mongoose.Types.ObjectId(),
+                guildId: message.guild.id,
+                guildName: message.guild.name,
+                guildIcon: message.guild.iconURL() ? message.guild.iconURL() : "None."
+            })
+            await guildProfile.save().catch(console.error)
+            await Reply.send(message, {
+                content: `Server Name: ${guildProfile.guidName}`
+            })
+        } else {
+            await Reply.send(message, { content: `Server Id: ${guildProfile.guildId}` })
+        }
+    }
+}
