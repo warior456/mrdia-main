94f333937aff37d9f3921717fabb80b435d16247
Matteo <matteo.feyaerts@gmail.com>
2022-12-02 19:33:43 +0100

    refactor savefavorite

--- src/commands/buttons/savefavorite.js
+++ src/commands/buttons/savefavorite.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const User = require('../../schemas/user')
 const Reply = require('../../Structures/Handlers/replyHandler')
-
+//possible to move this to functions for adding /command and command for this
 module.exports = {
     name: "savefavorite",
     isButton: true,
@@ -9,32 +9,37 @@ module.exports = {
         await Reply.defer(message, false)
         let guildQueue = client.player.getQueue(message.guild.id);
         if (!guildQueue) {
-            return Reply.editReply(message, {content: `There is no song playing!`, ephemeral: true})
+            return Reply.editReply(message, { content: `There is no song playing!`, ephemeral: true })
         }
         let userProfile = await User.findOne({ userId: message.member.user.id })
-
         if (!userProfile) {
-            userProfile = await new User({
-                _id: mongoose.Types.ObjectId(),
-                userId: message.member.user.id,
-                userName: message.member.user.tag,
-                userIcon: "todo",
-                userFavoriteLinks: guildQueue.nowPlaying.url,
-                userFavoriteNames: guildQueue.nowPlaying.name,
-            })
-
-            await userProfile.save().catch(console.error)
-            console.log(userProfile)
-            await Reply.editReply(message, { content: `Added **${userProfile.userFavoriteNames[0]}** to your favorites!`, ephemeral: true })
-            
-        } else {
-            userProfile.userFavoriteLinks.unshift(guildQueue.nowPlaying.url)
-            userProfile.userFavoriteNames.unshift(guildQueue.nowPlaying.name)
-
-            await userProfile.save().catch(console.error)
-            await Reply.editReply(message, { content: `Added **${userProfile.userFavoriteNames[0]}** to your favorites!`, ephemeral: true })
-            console.log(userProfile)
+            userProfile = await createUser(message, guildQueue)
         }
+        prepareFavorite(message, userProfile, guildQueue)
     }
 }
 
+async function prepareFavorite(message, userProfile, guildQueue) {
+    if (!userProfile.userFavoriteLinks.includes(guildQueue.nowPlaying.url)) {
+        userProfile.userFavoriteLinks.unshift(guildQueue.nowPlaying.url)
+        userProfile.userFavoriteNames.unshift(guildQueue.nowPlaying.name)
+        saveFavorite(message, userProfile)
+    } else {
+        Reply.editReply(message, { content: `**${userProfile.userFavoriteNames[0]}** is already a favorite!`, ephemeral: true })
+    }
+}
+
+async function createUser(message, guildQueue) {
+    userProfile = await new User({
+        _id: mongoose.Types.ObjectId(),
+        userId: message.member.user.id,
+        userName: message.member.user.tag,
+        userIcon: "todo",
+    })
+    return userProfile
+}
+
+async function saveFavorite(message, userProfile) {
+    await userProfile.save().catch(console.error)
+    await Reply.editReply(message, { content: `Added **${userProfile.userFavoriteNames[0]}** to your favorites!`, ephemeral: true })
+}
