1bc78804a2f57e277667a2b5de3c1f9319e02543
Matteo <matteo.feyaerts@gmail.com>
2023-05-26 19:33:51 +0200

    fix bypass for botowner

--- src/functions/music/skipFunction.js
+++ src/functions/music/skipFunction.js
@@ -1,28 +1,33 @@
 async function skipSong(userId, queue) {
 	song = queue.songs[0];
 
-    let userCount = queue.voiceChannel.members.size;
+	let userCount = queue.voiceChannel.members.size;
 	let userR = Math.floor(userCount / 2);
 
+	if (message.member.user.id == client.config.owner) return skip(queue, song);
+	
 	if (song.metadata.skipVotes.includes(userId))
 		return { content: `you already voted, ${song.metadata.skipVotes.length}/${userR}`, ephemeral: true };
 
 	await song.metadata.skipVotes.push(userId);
 
-
 	if (song.metadata.skipVotes.length >= userR) {
-		if(!queue.songs[1]){
-			queue.stop()
-			return 'Skipped song'
-		} 
-		skippedTo = await queue.skip();
-		skipMsg = `Skipped song, Now playing: **${skippedTo.name}**`;
-		song.metadata.skipVotes = []
-		return skipMsg;
+		return skip(queue, song);
 	} else {
 		skipMsg = `Voted (skip): ${song.metadata.skipVotes.length}/${userR}`;
 		return skipMsg;
 	}
+}
+
+async function skip(queue, song) {
+	if (!queue.songs[1]) {
+		queue.stop();
+		return "Skipped song";
+	}
+	skippedTo = await queue.skip();
+	skipMsg = `Skipped song, Now playing: **${skippedTo.name}**`;
+	song.metadata.skipVotes = [];
+	return skipMsg;
 }
 
 module.exports = {
