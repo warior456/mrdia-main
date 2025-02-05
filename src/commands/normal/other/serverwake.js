8450d30c98e0dcce40aa5c78a07e38a2c274e34d
Matteo_fey <matteo.feyaerts@gmail.Com>
2025-02-05 12:54:41 +0100

    add another channel

--- src/commands/normal/other/serverwake.js
+++ src/commands/normal/other/serverwake.js
@@ -8,7 +8,7 @@ module.exports = {
 	category: "other",
 	noHelp: true,
 	run: (client, message, args) => {
-		if ((message.channel.id != "998168141911822376")&&(message.channel.id != "1273971416966103124")) {
+		if ((message.channel.id != "998168141911822376")&&(message.channel.id != "1273971416966103124")&&(message.channel.id != "1336666205037137993")) {
 			return Reply.send(message, "Wrong channel!, this is a private command and can only be used in the #serverwake channel in some servers");
 		}
 		Reply.send(message, "Waking server...")
