cc79df556c8d84fea91db53c39b694a810d065df
Matteo_fey <matteo.feyaerts@gmail.Com>
2025-09-08 23:31:18 +0200

    bugfix

--- src/commands/buttons/remove_messageButton.js
+++ src/commands/buttons/remove_messageButton.js
@@ -1,4 +1,5 @@
 const Reply = require("../../structures/handlers/replyHandler")
+const { MessageFlags } = require("discord.js");
 
 module.exports = {
     name : 'remove_message',
@@ -6,6 +7,12 @@ module.exports = {
     //ownerOnly: true,
     run : async(client, message) => {
         await Reply.deferUpdate(message) //Reply.follow to send messages and Reply.editReply to edit current message
+        if(message.message.type == 20){//check for efemeral message
+            Reply.follow(message, { content: `The message you want to delete is Ephemeral please press "Dismiss message"\n If you believe this is not the case please contact the bot owner`, flags: MessageFlags.Ephemeral })
+            return
+        } 
+
         message.message.delete()
+        console.log(message.message.type)
     }
 }
