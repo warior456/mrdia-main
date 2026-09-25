e91399ce217579ddd0aac5469a1b9daf5567493c
Matteo_fey <66562258+warior456@users.noreply.github.com>
2026-09-25 13:51:21 +0200

    improve stop command

--- src/commands/normal/music/stop.js
+++ src/commands/normal/music/stop.js
@@ -1,14 +1,51 @@
 const Reply = require("../../../structures/handlers/replyHandler");
-const { ButtonBuilder, ActionRowBuilder, ButtonStyle, ApplicationCommandOptionType } = require("discord.js");
+
 module.exports = {
 	name: "stop",
 	aliases: ["leave"],
 	description: "Stop the queue",
 	category: "music",
-	run: (client, message, args) => {
+	run: async (client, message, args) => {
 		const queue = client.distube.getQueue(message);
-		if (!queue) return Reply.send(message, { content: `There is nothing playing right now!`, ephemeral: true });
-		queue.stop().then(Reply.send(message, { content: `Stopped the queue!` }));
-		queue.voice.leave()
+		const botVoiceChannel = message.guild?.members?.me?.voice?.channel;
+		const distubeVoice = client.distube.voices.get(message);
+		const isInVoice = Boolean(botVoiceChannel || distubeVoice);
+
+		if (!queue && !isInVoice) {
+			return Reply.send(message, { content: "There is nothing playing right now!", ephemeral: true });
+		}
+
+		const memberVoiceChannel = message.member?.voice?.channel;
+		if (botVoiceChannel && memberVoiceChannel && memberVoiceChannel.id !== botVoiceChannel.id && message.member?.user?.id !== client.config.owner) {
+			return Reply.send(message, { content: "You must be in the same voice channel as the bot!", ephemeral: true });
+		}
+
+		try {
+			if (queue) {
+				await queue.stop();
+			}
+
+			if (distubeVoice) {
+				distubeVoice.leave();
+			} else if (queue?.voice) {
+				queue.voice.leave();
+			} else {
+				client.distube.voices.leave(message);
+			}
+
+			if (message.guild?.members?.me?.voice?.channel) {
+				await message.guild.members.me.voice.disconnect().catch(() => {});
+			}
+
+			if (queue) {
+				Reply.send(message, { content: "Stopped the queue!" });
+			} else {
+				Reply.send(message, { content: "Left the voice channel!" });
+			}
+		} catch (error) {
+			console.error("Error in stop command:", error);
+			Reply.send(message, { content: "An error occurred while stopping!", ephemeral: true });
+		}
 	},
 };
+
